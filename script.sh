884394d8bbc7a41e22f43297a2487b765cff4ae1
congtusoma146 <quangduy06.96@gmail.com>
2022-11-01 21:03:23 +0700

    update

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
 #! bin/sh
-
-echo hello world, from a script file!
+sudo docker update
+sudo service docker start
+docker pull congtusoma146/techworld:v-0.77
+docker-compose -f docker-compose.yaml up
+cd /home/app
+npm install
+node server.js
